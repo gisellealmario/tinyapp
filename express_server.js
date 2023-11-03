ff61c926f4e1cd038b8c3e0be2c98824cd8709e9
gisellealmario <gisellealmario@gmail.com>
2023-11-03 07:58:35 +0000

    greeting&url

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+const templateVars = { greeting: "Hello World!" };
 //in the localhost:8080 web shows Hello!
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,16 +19,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 // in http://localhost:8080/hello. shows hello world
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
  });
  
-
+ app.get("/urls", (req, res) => { //to render the urls_index.ejs template and pass the urlDatabase to it.
+  res.render("urls_index", { urlDatabase: urlDatabase });
+});
+app.get("/hello", (req, res) => {
+  res.render("hello_world",{templateVars: templateVars});
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
