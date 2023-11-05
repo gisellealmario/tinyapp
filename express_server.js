81961c8fbcb848b316216eb166d09cbd3df194f6
gisellealmario <gisellealmario@gmail.com>
2023-11-05 08:46:22 +0000

    registration

--- express_server.js
+++ express_server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-function generateRandomString() {}
+function generateRandomString() {
+  
+}
 
 app.set("view engine", "ejs");
 
@@ -13,77 +16,109 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id]; // Retrieve the long URL from urlDatabase
-  
-  const templateVars = { id: id, longURL: longURL };
-  res.render("urls_show", templateVars);
-  
-});
-const templateVars = { greeting: "Hello World!" };
-//in the localhost:8080 web shows Hello!
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//in the http://localhost:8080/urls.json shows the URL
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-//in http://localhost:8080/hello. shows hello world
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/urls", (req, res) => { //to render the urls_index.ejs template and pass the urlDatabase to it.
-  res.render("urls_index", { urls: urlDatabase });
-});
-app.get("/hello", (req, res) => {
-  res.render("hello_world",{templateVars: templateVars});
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+    // ... any other vars
+  };
+  res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+  res.render("urls_new", templateVars);
+});
 
-app.post("/login", (req, res) => {
-  const username = req.body.username; // Get the username from the request body
-  res.cookie('username', username); // Set the username in a cookie
-  res.redirect("/urls"); // Redirect back to the /urls page
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]; 
+
+  const templateVars = { id: id, longURL: longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id]; // Retrieve the long URL from urlDatabase
+  const longURL = urlDatabase[id]; 
   res.redirect(longURL);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok"); 
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username; 
+  res.cookie('username', username); 
+  res.redirect("/urls");
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls"); 
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   if (urlDatabase[id]) {
     delete urlDatabase[id];
-    res.redirect("/urls"); 
+    res.redirect("/urls");
   }
 });
+
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const newLongURL = req.body.longURL; 
+  const newLongURL = req.body.longURL;
   urlDatabase[id] = newLongURL;
   res.redirect("/urls");
+});
+
+app.get("/register", (req, res) => {
+  res.render("registration");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
